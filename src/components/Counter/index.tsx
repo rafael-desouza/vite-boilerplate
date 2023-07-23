b631b5a5e2f9cab1734647293982e5388d0f870b
rafamenegheti <rafaelferreiradesouza7@gmail.com>
2023-07-23 11:10:13 -0300

    chore: configured eslint and prettier

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,17 +1,14 @@
-import { useState } from "react"
-
-
+import { useState } from 'react'
 
 export const Counter = () => {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
-    return (
-        <>
-            <h1>Count <span>{count}</span></h1>
-            <button onClick={() => setCount (count + 1)}>
-
-                Increment
-            </button>
-        </>
-    )
+  return (
+    <>
+      <h1>
+        Count <span>{count}</span>
+      </h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </>
+  )
 }
