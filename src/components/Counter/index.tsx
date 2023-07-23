0be103802a19185e66675ee3c72484e8bc2284c4
rafamenegheti <rafaelferreiradesouza7@gmail.com>
2023-07-23 10:51:45 -0300

    feat: initial configuration

---
+++ src/components/Counter/index.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+
+
+export const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <h1>Count <span>{count}</span></h1>
+            <button onClick={() => setCount (count + 1)}>
+
+                Increment
+            </button>
+        </>
+    )
+}
